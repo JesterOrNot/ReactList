3756deb0b20d868ab7cf87d126944163ffcf7f39
Sean Hellum <seanhellum45@gmail.com>
2020-04-07 04:17:19 +0000

    up

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -3,6 +3,8 @@ export default function listItem(props) {
     return(
         <li>
             <input type="checkbox"/>
+            <label>{ props.todo }</label>
+            <button className="delete">Delete</button>
         </li>
     )
 }
