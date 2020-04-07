7009ac01c46535ff294bc20ca819f99d41722896
Sean Hellum <seanhellum45@gmail.com>
2020-04-07 03:42:26 +0000

    Hello World

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from "react-dom";
+
+const App = () => {
+    return (
+        <h1>Hello World From React.JS</h1>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
